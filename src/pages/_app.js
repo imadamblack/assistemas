e8b5ed9e4458e616567f53e48d21a53890d80f66
Adam Black <fernando@notoriovs.com>
2025-04-03 18:57:32 -0600

    UPD: set lead_utm cookie and sent on opt-in-form submit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,17 +4,22 @@ import { useRouter } from 'next/router';
 import { setCookie, getCookie } from 'cookies-next';
 
 function MyApp({Component, pageProps}) {
-  const {query: { fbclid }} = useRouter();
+  const {query: {fbclid, utm_source, utm_medium, utm_campaign, utm_content}} = useRouter();
   const _fbc = getCookie('_fbc');
 
+  const date = new Date();
   if (!_fbc && fbclid) {
-    const date = new Date();
     setCookie(
       '_fbc',
       `fb.1.${Date.now()}.${fbclid}`,
-      {expires: new Date(date.setDate(date.getDate() + 7))}
+      {expires: new Date(date.setDate(date.getDate() + 7))},
     );
   }
+  setCookie(
+    'lead_utm',
+    {utm_source, utm_medium, utm_campaign, utm_content},
+    {expires: new Date(date.setDate(date.getDate() + 7))},
+  );
 
   return (
     <Layout>
