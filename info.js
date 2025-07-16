df6d0a58801f0c7be56031329ffc612c8274f112
Adam Black <fernando@notoriovs.com>
2025-07-16 11:22:07 -0600

    UPD: info.surveyRedirect

--- info.js
+++ info.js
@@ -30,5 +30,5 @@ export const info = {
   crmWebhook: "https://assistemas.mx/lead/obt_lead.php",
   surveyWebhook: 'https://hook.us1.make.com/1zvc4qo9r5q613vynfu4poe7evy1m8c6',
   partialSurveyWebhook: 'https://hook.us1.make.com/4ai3ugag59wqiicr77bfr8p7fnblley5',
-  surveyRedirect: 'https://scheduler.zoom.us/javier-s/javisierra',
+  surveyRedirect: 'https://scheduler.zoom.us/manuel-bre-a/reunion',
 };
